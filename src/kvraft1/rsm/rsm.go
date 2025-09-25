14e49afc05011c2a7953feb53ddca363bcef99d0
Beauuu <1569163626@qq.com>
2025-09-25 16:44:19 +0800

    lab4a: potential bug send after applyCh was closed

--- src/kvraft1/rsm/rsm.go
+++ src/kvraft1/rsm/rsm.go
@@ -2,24 +2,26 @@ package rsm
 
 import (
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
 	"6.5840/raft1"
 	"6.5840/raftapi"
 	"6.5840/tester1"
-
 )
 
 var useRaftStateMachine bool // to plug in another raft besided raft1
-
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Me  int
+	Req any
+	Id  int64
 }
-
 
 // A server (i.e., ../server.go) that wants to replicate itself calls
 // MakeRSM and must implement the StateMachine interface.  This
@@ -41,6 +43,9 @@ type RSM struct {
 	maxraftstate int // snapshot if log grows this big
 	sm           StateMachine
 	// Your definitions here.
+	opId            atomic.Int64
+	pendingOps      map[int]Op
+	pendingChannels map[int]chan any
 }
 
 // servers[] contains the ports of the set of
@@ -60,14 +65,18 @@ type RSM struct {
 // any long-running work.
 func MakeRSM(servers []*labrpc.ClientEnd, me int, persister *tester.Persister, maxraftstate int, sm StateMachine) *RSM {
 	rsm := &RSM{
-		me:           me,
-		maxraftstate: maxraftstate,
-		applyCh:      make(chan raftapi.ApplyMsg),
-		sm:           sm,
+		me:              me,
+		maxraftstate:    maxraftstate,
+		applyCh:         make(chan raftapi.ApplyMsg),
+		sm:              sm,
+		pendingOps:      make(map[int]Op, 0),
+		pendingChannels: make(map[int]chan any, 0),
 	}
+	rsm.opId.Store(0)
 	if !useRaftStateMachine {
 		rsm.rf = raft.Make(servers, me, persister, rsm.applyCh)
 	}
+	go rsm.reader()
 	return rsm
 }
 
@@ -75,6 +84,41 @@ func (rsm *RSM) Raft() raftapi.Raft {
 	return rsm.rf
 }
 
+// reader func
+func (rsm *RSM) reader() {
+	for {
+		select {
+		case applyMsg, ok := <-rsm.applyCh:
+			if !ok {
+				for _, ch := range rsm.pendingChannels {
+					close(ch)
+				}
+				return
+			}
+			if applyMsg.CommandValid {
+				op, _ := applyMsg.Command.(Op)
+				returned := rsm.sm.DoOp(op.Req)
+				rsm.mu.Lock()
+				if pendingOp, ok := rsm.pendingOps[applyMsg.CommandIndex]; ok {
+					if pendingOp.Me != op.Me && pendingOp.Id != op.Id {
+						for _, ch := range rsm.pendingChannels {
+							close(ch)
+						}
+						rsm.pendingChannels = make(map[int]chan any, 0)
+						rsm.pendingOps = make(map[int]Op, 0)
+					} else {
+						rsm.pendingChannels[applyMsg.CommandIndex] <- returned
+						delete(rsm.pendingOps, applyMsg.CommandIndex)
+						delete(rsm.pendingChannels, applyMsg.CommandIndex)
+					}
+				}
+				rsm.mu.Unlock()
+			}
+		default:
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
 
 // Submit a command to Raft, and wait for it to be committed.  It
 // should return ErrWrongLeader if client should find new leader and
@@ -86,5 +130,27 @@ func (rsm *RSM) Submit(req any) (rpc.Err, any) {
 	// is the argument to Submit and id is a unique id for the op.
 
 	// your code here
-	return rpc.ErrWrongLeader, nil // i'm dead, try another server.
+	opId := rsm.opId.Add(1)
+	op := Op{
+		Me:  rsm.me,
+		Req: req,
+		Id:  opId,
+	}
+	ch := make(chan any)
+	rsm.mu.Lock()
+	index, _, isLeader := rsm.rf.Start(op)
+	if !isLeader {
+		rsm.mu.Unlock()
+		return rpc.ErrWrongLeader, nil
+	}
+	rsm.pendingOps[index] = op
+	rsm.pendingChannels[index] = ch
+	rsm.mu.Unlock()
+
+	received, ok := <-ch
+	// currently, only if shutdown and leadership change will close all those pending channels
+	if !ok {
+		return rpc.ErrWrongLeader, nil
+	}
+	return rpc.OK, received
 }
